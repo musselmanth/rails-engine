fef27e298f92493aaf3c2875df217818140c7dde
Thomas Musselman <tmusselman@gmail.com>
2022-10-18 19:01:19 -0600

    test: merchant happy path

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -41,4 +41,29 @@ RSpec.describe "Merchants API" do
       expect(parsed_response[:data].empty?).to be(true)
     end
   end
+
+  context 'get single merchant' do
+
+    it 'returns the merchant data' do
+      merchant_model = create(:merchant)
+      get "/api/v1/merchants/#{merchant_model.id}"
+
+      expect(response).to be_successful
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body).to have_key(:data)
+      expect(response_body[:data]).to be_a(Hash)
+
+      merchant = response_body[:data]
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to eq(merchant_model.id.to_s)
+      expect(merchant[:type]).to eq("merchant")
+      expect(merchant[:attributes]).to be_a(Hash)
+      merch_attr = merchant[:attributes]
+      expect(merch_attr).to have_key(:name)
+      expect(merch_attr[:name]).to eq(merchant_model.name)
+    end
+  end
 end
